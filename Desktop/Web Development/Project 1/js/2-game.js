01044c42f526bb45819a3433c9eb89c2cdb2d4cf
Isabella Paiva <isabellanpaiva@gmail.com>
2023-07-21 18:08:54 +0200

    version0

---
+++ Desktop/Web Development/Project 1/js/2-game.js
@@ -0,0 +1,12 @@
+const Game = {
+    gameScreen: document.querySelector("#game-screen"),
+    gameSize: {
+        w: window.innerWidth,
+        h: window.innerHeight
+
+    }
+}
+
+document.body.figure.style.backgroundColor = "red";
+
+
